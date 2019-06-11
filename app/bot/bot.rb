969fc1b14337099e3a0e304925da5ecc4e15099c
Yacine CHALABI <yacine.chalabi@lanteas.com>
2019-06-11 14:18:01 +0200

    first commit

---
+++ app/bot/bot.rb
@@ -0,0 +1,7 @@
+require 'facebook/messenger'
+include Facebook::Messenger
+
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
+Bot.on :message do |messsage|
+	message.reply(text: "hello !")
+end
